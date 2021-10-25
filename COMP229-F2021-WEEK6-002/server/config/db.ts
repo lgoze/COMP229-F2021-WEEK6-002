9227d83fc22551e0cde95f4a71e9bbd1fc560ca0
lgoze <lgoze@my.centennialcollege.ca>
2021-10-24 21:43:58 -0400

    first commit

---
+++ COMP229-F2021-WEEK6-002/server/config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://localhost:27017/portfolio";
+export const RemoteURI = "mongodb+srv://thiago:centennial@cluster0.hdwgd.mongodb.net/portfolio?retryWrites=true&w=majority";
+export const HostName = "localhost";
+export const Secret = "NotSoSecret";
